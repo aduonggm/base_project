b910fc97c4557b25f7a1bae391635e14d98a6db6
DuongNV <aduonggm@gmail.com>
2023-08-24 15:15:46 +0700

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+//    id("com.android.application") version "8.1.0" apply false
+    alias(libs.plugins.android.gradle.plugin) apply false
+    alias(libs.plugins.kotlin.gradle.plugin) apply false
+//    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+}
